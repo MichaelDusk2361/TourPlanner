b897129f14b043a95714250123093a502091fa42
MarkusDuskanich <72270002+MarkusDuskanich@users.noreply.github.com>
2022-03-15 15:58:54 +0100

    start of datGRID IMPL

--- TourPlanner/TourPlanner.PL/ViewModel/LogsViewModel.cs
+++ TourPlanner/TourPlanner.PL/ViewModel/LogsViewModel.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using TourPlanner.Model;
 
 namespace TourPlanner.PL.ViewModel
 {
     public class LogsViewModel
     {
+        public IEnumerable<TourLog> Data { get; set; }
+
+        public LogsViewModel()
+        {
+            Data = new List<TourLog>()
+            {
+                new() { Date = DateTime.Now, Distance = 10, Duration = new(10) },
+                new() { Date = DateTime.Now, Distance = 10, Duration = new(10) },
+                new() { Date = DateTime.Now, Distance = 10, Duration = new(10) },
+                new() { Date = DateTime.Now, Distance = 10, Duration = new(10) },
+                new() { Date = DateTime.Now, Distance = 10, Duration = new(10) },
+            };
+        }
+
+        public void OnAutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            PropertyDescriptor propertyDescriptor = (PropertyDescriptor)e.PropertyDescriptor;
+            e.Column.Header = propertyDescriptor.DisplayName;
+            if (propertyDescriptor.DisplayName == nameof(ITEntity.Id))
+            {
+                e.Cancel = true;
+            }
+            else if (propertyDescriptor.DisplayName == nameof(ITEntity.Version))
+            {
+                e.Cancel = true;
+            }
+        }
     }
 }
