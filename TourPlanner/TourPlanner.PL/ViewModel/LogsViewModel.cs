3b2cde5bd4a3105b09c0c7dcfb979169116bde9c
MarkusDuskanich <72270002+MarkusDuskanich@users.noreply.github.com>
2022-03-16 11:55:58 +0100

    added databrid and bound properties to columns

--- TourPlanner/TourPlanner.PL/ViewModel/LogsViewModel.cs
+++ TourPlanner/TourPlanner.PL/ViewModel/LogsViewModel.cs
@@ -24,19 +24,5 @@ namespace TourPlanner.PL.ViewModel
                 new() { Date = DateTime.Now, Distance = 10, Duration = new(10) },
             };
         }
-
-        public void OnAutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
-        {
-            PropertyDescriptor propertyDescriptor = (PropertyDescriptor)e.PropertyDescriptor;
-            e.Column.Header = propertyDescriptor.DisplayName;
-            if (propertyDescriptor.DisplayName == nameof(ITEntity.Id))
-            {
-                e.Cancel = true;
-            }
-            else if (propertyDescriptor.DisplayName == nameof(ITEntity.Version))
-            {
-                e.Cancel = true;
-            }
-        }
     }
 }
