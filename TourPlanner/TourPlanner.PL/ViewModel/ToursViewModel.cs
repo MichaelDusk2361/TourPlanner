831b7f1973f553a314fe3323042d3f3ed4c1b1c3
MarkusDuskanich <72270002+MarkusDuskanich@users.noreply.github.com>
2022-03-22 15:53:15 +0100

    prepared for selected tour implementation

--- TourPlanner/TourPlanner.PL/ViewModel/ToursViewModel.cs
+++ TourPlanner/TourPlanner.PL/ViewModel/ToursViewModel.cs
@@ -4,13 +4,28 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using TourPlanner.Model;
+using TourPlanner.PL.View;
 
 namespace TourPlanner.PL.ViewModel
 {
 
     public class ToursViewModel : BaseViewModel
     {
+        private Tour? _selectedTour;
+        public Tour? SelectedTour
+        {
+            get => _selectedTour;
+            set
+            {
+                _selectedTour = value;
+                Console.WriteLine(SelectedTour?.Name);
+                //call event which updates tourdetailview and logsview
+            }
+        }
+
+
         private ObservableCollection<Tour>? _data;
         public ObservableCollection<Tour> Data
         {
