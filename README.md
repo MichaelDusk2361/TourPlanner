e747558936d7e1ce42f220363d281efbffdcc978
MarkusDuskanich <72270002+MarkusDuskanich@users.noreply.github.com>
2022-03-28 14:27:29 +0200

    added class to parse config json, updated readme config file instructions

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # TourPlanner
 
 to make the app work add a file named AppConfig.json to the same directory as the exe.\
-The file should contain a jason of this format:\
+The file should contain a json of this format:\
 {\
  &emsp;"Host": "localhost",\
  &emsp;"Username": "",\
  &emsp;"Password": "",\
  &emsp;"Database": "tourplannerdb",\
- &emsp;"IncludeErrorDetail": "true\false"\
+ &emsp;"IncludeErrorDetail": "true\false",\
+ &emsp;"MapQuestApiKey": ""\
 }
--- TourPlanner/TourPlanner.DAL/Context/TourPlannerContext.cs
+++ TourPlanner/TourPlanner.DAL/Context/TourPlannerContext.cs
@@ -4,14 +4,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+using TourPlanner.Common;
 
 namespace TourPlanner.DAL.Context
 {
     public class TourPlannerContext : DBContext
     {
-
         private static string? s_connectionString = null;
         public TourPlannerContext() : base(s_connectionString ?? ParseConnectionStringFromConfig())
         {
@@ -23,8 +21,7 @@ namespace TourPlanner.DAL.Context
         {
             try
             {
-                var configFile = File.ReadAllText("AppConfig.json");
-                var parsedConfig = JsonConvert.DeserializeObject<Dictionary<string, string>>(configFile);
+                var parsedConfig = ConfigFile.Parse("AppConfig.json");
                 //"Host= ;Username= ;Password= ;Database= ;IncludeErrorDetail= "
                 s_connectionString = $"Host={parsedConfig["Host"]};Username={parsedConfig["Username"]};Password={parsedConfig["Password"]};Database={parsedConfig["Database"]};IncludeErrorDetail={parsedConfig["IncludeErrorDetail"]}";
                 return s_connectionString;
