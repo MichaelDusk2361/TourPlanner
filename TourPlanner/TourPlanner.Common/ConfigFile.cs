efedf854f00ee3c7fffbb5cef032ac1d32e00120
MarkusDuskanich <72270002+MarkusDuskanich@users.noreply.github.com>
2022-03-28 14:43:28 +0200

    cleaned up error handling

--- TourPlanner/TourPlanner.Common/ConfigFile.cs
+++ TourPlanner/TourPlanner.Common/ConfigFile.cs
@@ -4,24 +4,11 @@ namespace TourPlanner.Common
 {
     public class ConfigFile
     {
-        public static Dictionary<string, string> Parse(string path)
+        public static Dictionary<string, string>? Parse(string path)
         {
-            try
-            {
-                var configFile = File.ReadAllText(path);
-                var parsedConfig = JsonConvert.DeserializeObject<Dictionary<string, string>>(configFile);
-                return parsedConfig;
-            }
-            catch (FileNotFoundException e)
-            {
-                Console.WriteLine($"{path} is missing");
-                throw e;
-            }
-            catch (ArgumentNullException e)
-            {
-                Console.WriteLine($"{path} has invalid format");
-                throw e;
-            }
+            var configFile = File.ReadAllText(path);
+            var parsedConfig = JsonConvert.DeserializeObject<Dictionary<string, string>>(configFile);
+            return parsedConfig;
         }
 
         public static bool TryParse(string path, out Dictionary<string, string>? parsedFile)
