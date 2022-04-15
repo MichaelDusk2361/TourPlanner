712b9f5bca8836d05e0e97539ff400bdd1829139
MarkusDuskanich <72270002+MarkusDuskanich@users.noreply.github.com>
2022-04-15 21:00:48 +0200

    added boilerplate code for menubar

--- TourPlanner/TourPlanner.PL/ViewModel/Main/MainViewModel.MenuBar.cs
+++ TourPlanner/TourPlanner.PL/ViewModel/Main/MainViewModel.MenuBar.cs
@@ -1,11 +1,47 @@
-﻿namespace TourPlanner.PL.ViewModel.Main
+﻿using System;
+
+namespace TourPlanner.PL.ViewModel.Main
 {
     public partial class MainViewModel
     {
         private void MenuBarSetup()
         {
-
+            AddImportEvent();
+            AddExportEvent();
+            AddToursSummaryEvent();
+            AddTourReportEvent();
         }
 
+        private void AddTourReportEvent()
+        {
+            MenuBar.TourReportEvent += (sender, e) =>
+            {
+
+            };
+        }
+
+        private void AddToursSummaryEvent()
+        {
+            MenuBar.ToursSummaryEvent += (sender, e) =>
+            {
+
+            };
+        }
+
+        private void AddExportEvent()
+        {
+            MenuBar.ExportEvent += (sender, e) =>
+            {
+
+            };
+        }
+
+        private void AddImportEvent()
+        {
+            MenuBar.ImportEvent += (sender, e) =>
+            {
+
+            };
+        }
     }
 }
--- TourPlanner/TourPlanner.PL/ViewModel/Sub/MenuBarViewModel.cs
+++ TourPlanner/TourPlanner.PL/ViewModel/Sub/MenuBarViewModel.cs
@@ -1,6 +1,38 @@
-﻿namespace TourPlanner.PL.ViewModel.Sub
+﻿using System;
+using System.Windows.Input;
+using TourPlanner.PL.Helper;
+
+namespace TourPlanner.PL.ViewModel.Sub
 {
     public class MenuBarViewModel : BaseViewModel
     {
+        public event EventHandler? ExportEvent = null;
+        public ICommand ExportCommand { get; }
+        public event EventHandler? ImportEvent = null;
+        public ICommand ImportCommand { get; }
+        public event EventHandler? ToursSummaryEvent = null;
+        public ICommand ToursSummaryCommand { get; }
+        public event EventHandler? TourReportEvent = null;
+        public ICommand TourReportCommand { get; }
+
+        public MenuBarViewModel()
+        {
+            ExportCommand = new RelayCommand((_) =>
+            {
+                ExportEvent?.Invoke(this, EventArgs.Empty);
+            });
+            ImportCommand = new RelayCommand((_) =>
+            {
+                ImportEvent?.Invoke(this, EventArgs.Empty);
+            });
+            ToursSummaryCommand = new RelayCommand((_) =>
+            {
+                ToursSummaryEvent?.Invoke(this, EventArgs.Empty);
+            });
+            TourReportCommand = new RelayCommand((_) =>
+            {
+                TourReportEvent?.Invoke(this, EventArgs.Empty);
+            });
+        }
     }
 }
